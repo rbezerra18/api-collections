943c366cb1a1049b17d9b1ccb3d8ecd05fe68caf
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-06 11:48:04 -0300

    Criar array de double (doubleArrayOf) e interar sobre ele: count, find e any.

--- src/main/kotlin/bezerra/roberto/collections/TesteOperacoes.kt
+++ src/main/kotlin/bezerra/roberto/collections/TesteOperacoes.kt
@@ -19,4 +19,18 @@ fun main() {
 
     salarioMaior2500.forEach { println(it) }
 
+    println("\n")
+
+    println(salarios.count{it in 2000.0..5000.0})
+
+    println("\n")
+
+    println(salarios.find{it == 2250.0})
+    println(salarios.find{it == 500.0})
+
+    println("\n")
+
+    println(salarios.any{it == 1000.0})
+    println(salarios.any{it == 500.0})
+
 }
