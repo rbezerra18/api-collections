6936845a25ae2852793b01c4f6a1268736a03e62
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-06 13:24:48 -0300

    Criar coleções mutáveis (mutableMap) e iterar sobre elas (funções: create, remove, findById e findAll).

--- src/main/kotlin/bezerra/roberto/collections/TesteMutavelMap.kt
+++ src/main/kotlin/bezerra/roberto/collections/TesteMutavelMap.kt
@@ -4,12 +4,23 @@ fun main() {
     val joao = Funcionario("João", 2000.0, "CLT")
     val pedro = Funcionario("Pedro", 1500.0, "PJ")
     val maria = Funcionario("Maria", 4000.0, "CLT")
-
+//criar objeto do tipo Repositório
     val repositorio = Repositorio<Funcionario>()
-
+//popular mapa
     repositorio.create(joao.nome, joao)
     repositorio.create(pedro.nome, pedro)
     repositorio.create(maria.nome, maria)
-
+//encontrar objeto no mapa
     println(repositorio.findById(maria.nome))
+
+    println("\n")
+//imprimir o mapa completo
+    repositorio.findAll().forEach { println(it) }
+
+    println("\n")
+//remover objeto no mapa
+    repositorio.remove(maria.nome)
+
+    repositorio.findAll().forEach { println(it) }
+
 }
