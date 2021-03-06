a502e23830ef004f020c33090ea9aa3667134bbf
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-06 12:14:42 -0300

    Criar listOf usando data class Funcionario e iterar sobre ela (sortedBy).

--- src/main/kotlin/bezerra/roberto/collections/TesteList.kt
+++ src/main/kotlin/bezerra/roberto/collections/TesteList.kt
@@ -1,8 +1,8 @@
 package bezerra.roberto.collections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -10,8 +10,20 @@ fun main() {
     funcionarios.forEach { println(it) }
 
     println("\n")
-
+//encontrar Maria
     println(funcionarios.find{it.nome == "Maria"})
+
+    println("\n")
+//ordenar por salário
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("\n")
+//ordenar por nome
+    funcionarios
+        .sortedBy { it.nome }
+        .forEach { println(it) }
 
 }
 
