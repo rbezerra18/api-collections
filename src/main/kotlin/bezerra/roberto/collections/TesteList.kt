597f758ab1d5dbafc4cbd0d84d65af7eeb9ab751
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-06 12:22:50 -0300

    Criar listOf usando data class Funcionario e iterar sobre ela (groupBy).

--- src/main/kotlin/bezerra/roberto/collections/TesteList.kt
+++ src/main/kotlin/bezerra/roberto/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package bezerra.roberto.collections
 
 fun main() {
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -25,11 +25,21 @@ fun main() {
         .sortedBy { it.nome }
         .forEach { println(it) }
 
+    println("\n")
+//ordenar por tipocontratacao
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
 }
 
-data class Funcionario(val nome: String, val salario: Double){
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String)
+{
     override fun toString(): String =
         """
-            Nome: $nome           Salário: $salario                       
+        Nome: $nome           Salário: $salario                       
         """.trimIndent()
 }
